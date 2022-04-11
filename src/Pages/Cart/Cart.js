3293b1dc0916d3039778689e929e24e948943e5a
Muhsin Deniz <muhsin.deniz@arneca.com>
2022-04-11 19:15:42 +0300

    edited

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -13,6 +13,9 @@ const Cart = () => {
     let { user } = useContext(CompanySettingsContext);
     let [discountCart, setDiscountCart] = useState(0)
     let [loading, setLoading] = useState(false);
+    let [couponCode, setCouponCode] = useState(null)
+    let [discount, setDiscount] = useState(null)
+    let [lastPrice, setLastPrice] = useState(null)
 
     let priceProgress = (count, id) => {
         setBasket([...basket.map(data => {
@@ -31,6 +34,7 @@ const Cart = () => {
                 return { ...data }
             }
         })])
+
         UPDATE_DB_QUANTITY([...basket.map(data => {
             if (data._id === id) {
                 return {
@@ -96,87 +100,128 @@ const Cart = () => {
         history.push('/delivery')
     }
 
+    let sendCouponCode = () => {
+        if (couponCode === null) {
+            message.info("Lütfen bir kupon kodu giriniz!")
+        } else {
+            setLoading(true)
+            axios.post(`http://localhost:3000/api/coupon`, {
+                coupon: couponCode
+            })
+                .then(response => {
+                    if (response.data.result_message.type === "info") {
+                        message.info(response.data.result_message.message)
+                    } else if (response.data.result_message.type === "error")
+                        message.error(response.data.result_message.message)
+                    else {
+                        message.success(response.data.result_message.message)
+                    }
+                    setDiscount(response.data.result)
+                    setLoading(false)
+                })
+                .catch(error => {
+                    console.log(error)
+                    setLoading(false)
+                })
+        }
+    }
+
+    useEffect(() => {
+        setLastPrice((Number(discountCart) + (basket.length > 0 ? 15 : 0)) - Number(discount))
+    }, [discount])
+
+    console.log(discount)
+    console.log(lastPrice)
+
     return (
         <>
             <HeaderBanner page="Cart" targetPage="Shopping Cart" />
 
             <div className="shopping_cart_area mt-70">
                 <div className="container">
-                    <form action="#">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="table_desc">
+                                <div className="cart_page">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th className="product_remove">Delete</th>
+                                                <th className="product_thumb">Image</th>
+                                                <th className="product_name">Product</th>
+                                                <th className="product-price">Price</th>
+                                                <th className="product_quantity">Quantity</th>
+                                                <th className="product_total">Total</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            {
+                                                basket && basket.map((bask, index) => (
+                                                    <tr key={index}>
+                                                        <td className="product_remove"><a><i onClick={() => removeBasket(bask._id)} className="far fa-trash-alt"></i></a></td>
+                                                        <td className="product_thumb"><a href="#"><img src={`http://localhost:3000/${bask?.avatar}`} alt="" /></a></td>
+                                                        <td className="product_name"><a href="#">{bask.name}</a></td>
+                                                        <td className="product-price">${bask.newPrice}</td>
+                                                        <td className="product_quantity"><label>Quantity</label> <input onChange={(e) => priceProgress(e.target.value, bask._id)} min="1" max="100" defaultValue={bask?.quntity || 1} type="number" /></td>
+                                                        <td className="product_total">${Number(bask.newPrice * bask.quntity).toFixed(2)}</td>
+                                                    </tr>
+                                                ))
+                                            }
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="coupon_area">
                         <div className="row">
-                            <div className="col-12">
-                                <div className="table_desc">
-                                    <div className="cart_page">
-                                        <table>
-                                            <thead>
-                                                <tr>
-                                                    <th className="product_remove">Delete</th>
-                                                    <th className="product_thumb">Image</th>
-                                                    <th className="product_name">Product</th>
-                                                    <th className="product-price">Price</th>
-                                                    <th className="product_quantity">Quantity</th>
-                                                    <th className="product_total">Total</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-
-                                                {
-                                                    basket && basket.map((bask, index) => (
-                                                        <tr key={index}>
-                                                            <td className="product_remove"><a><i onClick={() => removeBasket(bask._id)} className="far fa-trash-alt"></i></a></td>
-                                                            <td className="product_thumb"><a href="#"><img src={`http://localhost:3000/${bask?.avatar}`} alt="" /></a></td>
-                                                            <td className="product_name"><a href="#">{bask.name}</a></td>
-                                                            <td className="product-price">${bask.newPrice}</td>
-                                                            <td className="product_quantity"><label>Quantity</label> <input onChange={(e) => priceProgress(e.target.value, bask._id)} min="1" max="100" defaultValue={bask?.quntity || 1} type="number" /></td>
-                                                            <td className="product_total">${Number(bask.newPrice * bask.quntity).toFixed(2)}</td>
-                                                        </tr>
-                                                    ))
-                                                }
-
-                                            </tbody>
-                                        </table>
+                            <div className="col-lg-6 col-md-6">
+                                <div className="coupon_code left">
+                                    <h3>Coupon</h3>
+                                    <div className="coupon_inner">
+                                        <p>Enter your coupon code if you have one.</p>
+                                        <input style={{ textTransform: "uppercase" }} onChange={(e) => setCouponCode(e.target.value.toUpperCase())} placeholder="Coupon code" type="text" />
+                                        <button onClick={() => sendCouponCode()}>Apply coupon</button>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="coupon_area">
-                            <div className="row">
-                                <div className="col-lg-6 col-md-6">
-                                    <div className="coupon_code left">
-                                        <h3>Coupon</h3>
-                                        <div className="coupon_inner">
-                                            <p>Enter your coupon code if you have one.</p>
-                                            <input placeholder="Coupon code" type="text" />
-                                            <button type="submit">Apply coupon</button>
+                            <div className="col-lg-6 col-md-6">
+                                <div className="coupon_code right">
+                                    <h3>Cart Totals</h3>
+                                    <div className="coupon_inner">
+                                        <div className="cart_subtotal">
+                                            <p>Subtotal</p>
+                                            <p className="cart_amount">${Number(discountCart).toFixed(2)}</p>
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 col-md-6">
-                                    <div className="coupon_code right">
-                                        <h3>Cart Totals</h3>
-                                        <div className="coupon_inner">
-                                            <div className="cart_subtotal">
-                                                <p>Subtotal</p>
-                                                <p className="cart_amount">${Number(discountCart).toFixed(2)}</p>
-                                            </div>
-                                            <div className="cart_subtotal">
-                                                <p>Calculate shipping</p>
-                                                <p className="cart_amount">${basket.length > 0 ? 15 : 0}</p>
-                                            </div>
+                                        <div className="cart_subtotal">
+                                            <p>Calculate shipping</p>
+                                            <p className="cart_amount">${basket.length > 0 ? 15 : 0}</p>
+                                        </div>
 
-                                            <div className="cart_subtotal">
-                                                <p>Total</p>
-                                                <p className="cart_amount">${(Number(discountCart) + (basket.length > 0 ? 15 : 0)).toFixed(2)}</p>
-                                            </div>
-                                            <div className="checkout_btn">
-                                                <a onClick={() => checkOut()} className="text-white">Proceed to Checkout</a>
-                                            </div>
+                                        <div className="cart_subtotal">
+                                            <p>Price</p>
+                                            <p className="cart_amount">$<del>{(Number(discountCart) + (basket.length > 0 ? 15 : 0))}</del></p>
+                                        </div>
+
+                                        <div className="cart_subtotal">
+                                            <p>Discount amount</p>
+                                            <p className="cart_amount">${discount}</p>
+                                        </div>
+
+                                        <div className="cart_subtotal">
+                                            <p>Total</p>
+                                            <p className="cart_amount">${lastPrice}</p>
+                                        </div>
+                                        <div className="checkout_btn">
+                                            <a onClick={() => checkOut()} className="text-white">Proceed to Checkout</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
 
